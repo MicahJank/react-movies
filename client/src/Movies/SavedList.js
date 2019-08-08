d0476f9b429aa7c79f940fb497eb346be8eed686
MicahJank <MicahJank@users.noreply.github.com>
2019-08-07 23:34:36 -0500

    fixed some styling issues

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const SavedList = props => (
-  <Link to={'/saved'}>
-    <div className="saved-list">
-      <h3>Saved Movies:</h3>
-      {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
-      ))}
-      <Link to={'/'} className="home-button">Home</Link>
-    </div>
-  </Link>
+  <div className="top">
+    <Link to={'/'} className="home-button">Home</Link>
+    <Link to={'/saved'} className="saved-list-movies">
+      <div className="saved-list">
+        <h3>Saved Movies:</h3>
+        {props.list.map(movie => (
+          <span className="saved-movie">{movie.title}</span>
+        ))}
+      </div>
+    </Link>
+  
+  </div>
 );
 
 export default SavedList;
